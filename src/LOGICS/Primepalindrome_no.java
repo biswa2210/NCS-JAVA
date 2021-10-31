b2c9b6869ef83633d3b0d70a1b9274983b5227da
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-10-31 18:35:34 +0530

    CREATED BY BISWA

--- src/LOGICS/Primepalindrome_no.java
+++ src/LOGICS/Primepalindrome_no.java
@@ -2,6 +2,11 @@ package LOGICS;
 import LOGICS.Prime_no;
 import LOGICS.Palindrome_no;
 
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 public class Primepalindrome_no
 {
     public boolean isprimepalindrome(int n)
