a5008cc0b855ea5482b1711ba52d2e0451112bdd
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-10-31 18:27:28 +0530

    CREATED BY BISWA

--- src/LOGICS/Amstrong_no.java
+++ src/LOGICS/Amstrong_no.java
@@ -1,5 +1,10 @@
 package LOGICS;
 
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 public class Amstrong_no {
     public  boolean isAmstrong(int n)
     {
