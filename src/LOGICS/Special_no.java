c6612d88b7f0af293dbbac922b79cfc914482aa2
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-10-31 18:34:08 +0530

    CREATED BY BISWA

--- src/LOGICS/Special_no.java
+++ src/LOGICS/Special_no.java
@@ -1,5 +1,10 @@
 package LOGICS;
 
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 public class Special_no
 {
     public int digit_factorial(int n)
