ce280c667194d410b2116a47ae0e1a443ebb77ef
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-10-31 18:32:37 +0530

    CREATED BY BISWA

--- src/LOGICS/Krishnamurthynum.java
+++ src/LOGICS/Krishnamurthynum.java
@@ -1,4 +1,10 @@
 package LOGICS;
+
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 import java.util.Scanner;
 public class Krishnamurthynum {
     public boolean isKrishnamurthy(int a)
